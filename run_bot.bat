4b78d26378de871eaa9e0e426bb7899548e0f674
Serenada1 <kostyuch@inbox.ru>
2025-08-04 02:19:05 +0300

    Create run_bot.bat

---
+++ run_bot.bat
@@ -0,0 +1,11 @@
+@echo off
+title Academy Invoker Bot
+echo Запуск бота Academy Invoker...
+echo.
+echo Убедитесь, что вы установили все зависимости:
+echo pip install -r requirements.txt
+echo.
+echo Запуск бота...
+python bot.py
+echo.
+pause
